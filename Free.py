8546657938ad5e0e6e5f923a06d8c64ff3883d26
Bryce Davis <brycedavis66@gmail.com>
2018-06-30 22:37:58 +0900

    new loops and art

--- Free.py
+++ Free.py
@@ -1,4 +1,35 @@
-mystring = 'Hello World'
-print(mystring + "100")
-print(len(mystring))
-print(type(mystring))
+x = b'yeehaw'
+print(x)
+print(type(x))
+print(str(10.09))
+array1 = ["happy", "sad", "gay", "cool", "c00cky!"]
+print(array1[1: 3])
+array1.append("Lovely")
+print(array1[-1])
+
+
+def BigPrint(n):
+    k = n
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k -= 1
+        for b in range(0, i + 1):
+            print("|", end="")
+        print("\r")
+
+
+num1 = int(input("how many lines do you want?: "))
+
+BigPrint(num1)
+
+
+def BigPrintF(n):
+
+    for i in range(0, n):
+        for j in range(0, i + 1):
+            print("|", end="")
+        print("\r")
+
+
+BigPrintF(num1)
